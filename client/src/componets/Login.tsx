056cd04cb1a3d3e03e1e2fcbc34ab5943b88df8a
Martin Pavlov <martin_pavlov2013@abv.bg>
2020-04-12 22:29:14 +0300

    Setting up typescript on client

---
+++ client/src/componets/Login.tsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class Login extends Component {
+    render() {
+        return (
+            <div>Login works</div>
+        );
+    }
+}
+
+export default Login;
