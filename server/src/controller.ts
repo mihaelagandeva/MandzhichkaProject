5f5687ef76ee4a1659669750bfe763a1f444e2f8
Martin Pavlov <martin_pavlov2013@abv.bg>
2020-05-17 23:07:13 +0300

    Fixing problems with the login and register end-pointhandlers

--- server/src/controller.ts
+++ server/src/controller.ts
@@ -1,14 +1,23 @@
 import { Request, Response } from "express";
 import User from "./db-config/models/user";
+import bcrypt from 'bcrypt';
 
 export let login = async (req: Request, res: Response) => {
-    await User.findOne({ username: "user1" }, (err: any, user: any[]) => {
+    const {username, password} = req.body;
+
+    await User.findOne({username: username}, (err: any, user: any) => {
         if (err) {
             res.status(501).send("Error!");
-        } else if (!user) {
-            res.status(401).send("Потребителското име или парола са грешни");
         } else {
-            res.send(user);
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (err) {
+                    res.status(500).send('Error');
+                } else if (result) {
+                    res.send(user);
+                } else {
+                    res.status(401).send("Потребителското име или парола са грешни");
+                }
+            });
         }
     }).exec();
 };
@@ -21,12 +30,20 @@ export let registration = async (req: Request, res: Response) => {
             return;
         }
 
-        await User.create({ username: req.body.username, password: req.body.password }, function (err: any, user: any) {
-            if (err) {
-                res.status(501).send("Error!");
+        await User.findOne({username: req.body.username}, (err: any, user: any) => {
+            if (!user) {
+                User.create({ username: req.body.username, password: req.body.password },
+                        function (err: any, user: any) {
+                    if (err) {
+                        res.status(501).send("Error!");
+                    } else {
+                        res.send();
+                    }
+                });
             } else {
-                res.send();
+                res.status(400).send('Потребителското име е вече заето!');
             }
         });
+
     };
 };
