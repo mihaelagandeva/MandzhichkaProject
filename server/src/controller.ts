f2c44502743b56f0e17b708d748296c5462d24fa
Martin Pavlov <martin_pavlov2013@abv.bg>
2020-07-20 13:37:01 +0300

    Fixing conflicts

--- server/src/controller.ts
+++ server/src/controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import User from "./db-config/models/user";
+import Recipe from "./db-config/models/recipe";
+import Tag, {ITag} from "./db-config/models/tag";
+import bcrypt from 'bcrypt';
 import Restaurant from "./db-config/models/restaurant";
 
 export let login = async (req: Request, res: Response) => {
@@ -30,6 +33,107 @@ export let registration = async (req: Request, res: Response) => {
             }
         });
     };
+};
+
+export let createRecipe = async (req: Request, res: Response) => {
+    await Recipe.findOne({ name: req.body.name, author: req.body.author },
+        async function (err: any, recipe: any) {
+            if (err) {
+                res.status(501).send("Error!");
+            } else {
+                if (recipe) {
+                    res.status(400).send();
+                } else {
+                    const tagsToBeInserted = await handleTags(req.body.tags, res);
+                    await Recipe.create({
+                        name: req.body.name,
+                        author: req.body.author,
+                        date: Date.now(),
+                        rating: 0,
+                        picturePath: req.body.picturePath,
+                        tags: tagsToBeInserted
+                    }, function (err: any, recipe: any) {
+                        if (err) {
+                            console.log(err)
+                            res.status(501).send("Error!");
+                        } else {
+                            res.status(201).send(recipe);
+                        }
+                    })
+                }
+            }
+        });
+};
+
+export let getRecipe = async (req: Request, res: Response) => {
+    await Recipe.findOne({ _id: req.params.recipeId },
+        function (err: any, recipe: any) {
+            if (err) {
+                res.status(501).send("Error!");
+            } else {
+                if (recipe) {
+                    res.status(200).send(recipe);
+                } else {
+                    res.status(404).send();
+                }
+            }
+        }
+    );
+};
+
+export let updateRecipe = async (req: Request, res: Response) => {
+    await Recipe.updateOne({ _id: req.params.recipeId }, { $set: req.body, $currentDate: { lastModified: true } },
+        function (err: any) {
+            if (err) {
+                res.status(501).send("Error!");
+            } else {
+                res.status(200).send();
+            }
+        }
+    );
+};
+
+export let deleteRecipe = async (req: Request, res: Response) => {
+    await Recipe.deleteOne({ _id: req.params.recipeId },
+        function (err: any) {
+            if (err) {
+                res.status(501).send("Error!");
+            } else {
+                res.status(204).send();
+            }
+        }
+    );
+};
+
+export let listAllRecipes = async (req: Request, res: Response) => {
+    const page = Number(req.params.page);
+    const size = Number(req.params.size);
+
+    if (page && size) {
+        const firstRecord = (page - 1) * size;
+        const search = req.params.search;
+        let recipes: any[] = [];
+        const query = {}; //should be added later
+
+        await Recipe.find(query, (err: any, result: any[]) => {
+            if(err) {
+                res.status(501).send("Error!");
+            } else {
+                recipes = result;
+            }
+        }).skip(firstRecord).limit(size);
+
+        const totalItems = await Recipe.find(query).countDocuments();
+
+        res.send({
+            page: page,
+            size: size,
+            resultSet: recipes,
+            totalItems: totalItems
+        });
+    } else {
+        res.status(400).send();
+    }
 };
 
 export let getRestaurants = async (req: Request, res: Response) => {
@@ -83,4 +187,51 @@ export let createRestaurant = async (req: Request, res: Response) => {
             });
         }
     });
+}
+
+export let createTags = async (req: Request, res: Response) => {
+    await Tag.create(req.body, function (err: any, tags: any) {
+        if (err) {
+            res.status(501).send("Error!");
+        } else {
+            res.status(201).send(tags);
+        }
+    });
+};
+
+export let listTags = async (_req: Request, res: Response) => {
+    await Tag.find(function (err: any, tags: any) {
+        if (err) {
+            res.status(501).send("Error!");
+        } else {
+            res.status(200).send(tags);
+        }
+    });
+};
+
+async function handleTags(tags: ITag[], res: Response) {
+    let result: any[] = [];
+    for (let i = 0; i < tags.length; i++) {
+        await Tag.findOne({ value: tags[i].value },
+            async function (err: any, tag: any) {
+                if (err) {
+                    res.status(501).send("Error!");
+                } else {
+                    if (tag) {
+                        result.push(tag);
+                    }
+                    else {
+                        await Tag.create(tags[i], function (err: any, tag: any) {
+                            if (err) {
+                                res.status(501).send("Error!");
+                            } else {
+                                result.push(tag);
+                            }
+                        })
+                    }
+                }
+            }
+        )
+    }
+    return result;
 }
