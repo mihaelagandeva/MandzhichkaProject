1512b6ec3275c8407aa31fc8c903d8ba47545c32
Martin Pavlov <martin_pavlov2013@abv.bg>
2020-07-23 10:45:46 +0300

    Updating the products model

--- server/src/db-config/models/product.ts
+++ server/src/db-config/models/product.ts
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
 
 const ProductSchema = new mongoose.Schema({
-    name: { type: String },
-    type: { type: String }
+    name: {
+        type: String,
+        maxlength: [30, 'Името на продукта трябва да е най-много 30 символа'],
+        minlength: [2, 'Името на продукта трябва да е най-малко 2 символа'],
+        required: [true, 'Името на продукта е задължително'],
+        unique: [true, 'Вече има продукт с такова име']
+    },
+    type: {
+        type: String,
+        maxlength: [30, 'Типа на продукта трябва да е най-много 30 символа'],
+        minlength: [2, 'Типа на продукта трябва да е най-малко 2 символа'],
+        required: [true, 'Типа на продукта е задъжителен'],
+    }
 });
 
 interface IProduct extends mongoose.Document {
