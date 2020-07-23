9f5a71df6c37b8e510bde7a8626cdaf7e0b68a13
Martin Pavlov <martin_pavlov2013@abv.bg>
2020-07-23 23:23:37 +0300

    Updating the event and course models

--- server/src/db-config/models/event.ts
+++ server/src/db-config/models/event.ts
@@ -1,17 +1,28 @@
 import mongoose from "mongoose";
 
 const EventSchema = new mongoose.Schema({
-    name: { type: String },
-    date: { type: Date },
-    address: { type: String },
+    name: {
+        type: String,
+        required: [true, 'Името на събитието е задължително'],
+        minlength: [3, 'Името на събитието трябва да е дълго поне 3 символа'],
+        maxlength: [100, 'Името на събитието е твърде дълго']
+    },
+    date: {
+        type: Date,
+        required: [true, 'Датата на събитието е задължителна']
+    },
+    address: {
+        type: String,
+        required: [true, 'Адресът на събитието е задължителен'],
+        minlength: [10, 'Адресът на събитието е твърде къс'],
+        maxlength: [150, 'Адресът на събитието е твърде дълъг']
+    },
     picturePath: { type: String },
     duration: {
         quantity: { type: Number },
         metric: { type: String }
     },
-    participants: [{
-        userId: { type: Number }
-    }]
+    participants: {type: Array}
 });
 
 interface IEvent extends mongoose.Document {
@@ -23,9 +34,7 @@ interface IEvent extends mongoose.Document {
         quantity: number;
         metric: string
     }
-    participants: {
-        userId: string
-    }[]
+    participants: string[]
 }
 
 const Event = mongoose.model<IEvent>("Event", EventSchema);
